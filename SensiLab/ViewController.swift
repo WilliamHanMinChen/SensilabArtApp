2d43e84147d7562c7865f8e58a15d3b0642cf617
HanMin Chen <Shww_wowo@live.cn>
2022-11-10 22:19:37 +1100

    Initial Commit

--- SensiLab/ViewController.swift
+++ SensiLab/ViewController.swift
@@ -7,18 +7,46 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate {
     
     @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //Setup the ARView
+        //Configuration for tracking images and objects
+        let configuration = ARWorldTrackingConfiguration()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        //Loads all the images its going to look for
+        let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)!
+        
+        
+        //Provide it to the configuration
+        configuration.detectionImages = referenceImages
+        
+        //Give it a maximum number it can track at the same time
+        configuration.maximumNumberOfTrackedImages = 10
+        
+        
+        // Set ARView delegate so we can define delegate methods in this controller
+        arView.session.delegate = self
+        
+        // Forgo automatic configuration to do it manually instead
+        arView.automaticallyConfigureSession = false
+        
+        // Disable any unneeded rendering options
+        arView.renderOptions = [.disableCameraGrain, .disableHDR, .disableMotionBlur, .disableDepthOfField, .disableFaceMesh, .disablePersonOcclusion, .disableGroundingShadows, .disableAREnvironmentLighting]
+        
+        //Run the session
+        arView.session.run(configuration)
+        
+        
     }
 }
